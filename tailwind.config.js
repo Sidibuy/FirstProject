7a114c148be6a0470416e0ad020e903a633fc065
Sidibuy <91381330+Sidibuy@users.noreply.github.com>
2022-09-09 02:05:31 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+// const defaultTheme = require("tailwindcss/defaultTheme");
+
+// module.exports = {
+//   content: ["/*.{html,js,css}"],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "split-white-black":
+//           "linear-gradient(to bottom, #111827 60% , white 40%);",
+//       },
+//       fontFamily: {
+//         sans: ["serif", ...defaultTheme.fontFamily.sans],
+//       },
+//       container: {
+//         center: true,
+//       },
+
+//       screens: {
+//         xs: "635px",
+//         sm: "640px",
+//         // => @media (min-width: 640px) { ... }
+
+//         md: "768px",
+//         // => @media (min-width: 768px) { ... }
+
+//         lg: "1024px",
+//         // => @media (min-width: 1024px) { ... }
+
+//         xl: "1280px",
+//         // => @media (min-width: 1280px) { ... }
+
+//         "2xl": "1536px",
+//         // => @media (min-width: 1536px) { ... }
+//       },
+//     },
+//   },
+// };
