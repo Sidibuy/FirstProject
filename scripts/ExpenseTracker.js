a01a4ba6a5b0e4239cc1e5840a4bfab2c02b41ab
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2024-01-13 23:49:12 +0800

    fix somthing again lol

--- scripts/ExpenseTracker.js
+++ scripts/ExpenseTracker.js
@@ -18,18 +18,25 @@ addExpense?.addEventListener("click", function () {
     caution.style.display = "none";
     var name = inputName.value;
     var date = inputDate.value;
-    var amount = inputAmount.value;
-    // var formattedAmount = inputAmount.toLocaleString("en-US").value;
+    var UnformattedInput = inputAmount.value;
+
+    // Format the value as currency using Intl.NumberFormat
+    const formattedValue = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "PHP", // You can change the currency code as needed
+    }).format(Number(UnformattedInput));
+
+    var amount = formattedValue;
 
     table.innerHTML += `
     <tr class="bg-sky-200">
-      <td class="border-solid border-2 border-blue-600" style="text-transform: capitalize">${name}</td>
-      <td class="border-solid border-2 border-blue-600">${date}</td>
-      <td class="border-solid border-2 border-blue-600">₱${amount}</td>
+      <td class="border-solid border-2 border-blue-600 px-1 py-2 my-1 text-transform: capitalize">${name}</td>
+      <td class="border-solid border-2 border-blue-600 px-1 py-2 my-1">${date}</td>
+      <td class="border-solid border-2 border-blue-600 px-1 py-2 my-1">${amount}</td>
       <td class="border-solid border-2 border-blue-600 text-center">
         <button 
           id="removeBtn"
-          class="fa-solid fa-trash-can bg-red-500 px-1 py-2 rounded"
+          class="fa-solid fa-trash-can bg-red-500 px-1 py-2 my-1 rounded"
           onclick="deleteItem(this)"></button>
       </td>
     </tr>`;
